8ba85348695e25d2f2a8f8fa1457aeef172c99c3
taboye1 <taccp21@gmail.com>
2021-09-21 22:11:17 -0400

    setup the design

--- app/src/main/java/temple/edu/signupform/FormActivity.kt
+++ app/src/main/java/temple/edu/signupform/FormActivity.kt
@@ -2,10 +2,18 @@ package temple.edu.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = findViewById(R.id.name_id)as EditText
+        val email = findViewById(R.id.email_id)as EditText
+        val passW = findViewById(R.id.passW_id)as EditText
+        val confPass = findViewById(R.id.passConf_id)as EditText
+        val btn = findViewById(R.id.button)as Button
     }
 }
